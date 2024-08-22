f0fd7a752db0c4621ce4717c568a7bab0f2a089d
LuizWatanabe2005 <167527845+LuizWatanabe2005@users.noreply.github.com>
2024-08-22 16:43:42 -0300

    Add files via upload

--- tela_inicial.php
+++ tela_inicial.php
@@ -13,7 +13,8 @@
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
+
+  </head>
 
 
 <body>
@@ -392,6 +393,39 @@
 
     </div>
 </div>
+
+<div id="containera"> 
+  <div class="batataa">
+
+        <div class="chega">
+            <h4>Comprar</h4>
+            <p>Comprar novo</p>
+            <p>Comprar usado</p>
+        </div>
+           
+        <div class="batata">
+            <h4>Vender</h4>
+            <p>Vender carro</p>
+            <p>Gerenciar meu Anuncio</p>
+        </div>
+    
+        <div class="gol">
+                <h4>Serviços</h4>
+                <p>tabela fipe</p>
+                <p>financiamento</p>
+        </div>
+    
+        <div class="jogo">
+                    <h4>Ajuda</h4>
+                    <p>para você</p>
+                    <p>Segurança</p>
+                    <p>Sobre nós</p>
+                    <p>Suporte</p>
+        </div>
+
+    </div>
+</div>
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
